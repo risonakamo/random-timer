d4fa8c580e32792435feee50a94dba1b6f3f2a38
risonakamo <hmykanae@gmail.com>
2021-08-27 23:54:16 -0400

    planning out time input

--- web/components/timer-input/timer-input.svelte
+++ web/components/timer-input/timer-input.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
   import "./timer-input.less";
+  import cx from "classnames";
+
+  var inEditing:boolean=false;
+  var topClass:Object;
+  var timeValue:string="000000";
+
+  /** on focus, enter editing mode */
+  function h_focus()
+  {
+    inEditing=true;
+  }
+
+  /** exit editing mode without saving */
+  function h_blur()
+  {
+    inEditing=false;
+  }
+
+  $:{
+    topClass={
+      editing:inEditing
+    };
+  }
 </script>
 
-<div class="timer-input">
+<div class={cx("timer-input",topClass)} on:focus={h_focus} on:blur={h_blur} tabindex="0">
   <span>5</span>
   <span class="small-digit">m</span>
   <div class="digit-spacer"></div>
