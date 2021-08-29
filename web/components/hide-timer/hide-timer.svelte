f20f25b9c87792fddd5b6be3ac407a5fac35adaa
risonakamo <hmykanae@gmail.com>
2021-08-28 20:55:02 -0400

    hide timer hide block

--- web/components/hide-timer/hide-timer.svelte
+++ web/components/hide-timer/hide-timer.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
   import TimerDisplay from "components/timer-display/timer-display.svelte";
 
+  var hidden:boolean=true;
+
   import "./hide-timer.less";
 </script>
 
 <div class="hide-timer">
-  <TimerDisplay timeValue="102" minDigitMode={true}/>
+  {#if !hidden}
+    <TimerDisplay timeValue="102" minDigitMode={true}/>
+  {:else}
+    <div class="hide-block">
+      <p>timer running</p>
+    </div>
+  {/if}
 </div>
